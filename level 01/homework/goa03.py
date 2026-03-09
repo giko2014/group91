727b7870f18bd64032475af8bf3d9e920443e2bd
giko2014 <g.sulikashvili@millennium-school.org>
2026-03-09 05:42:33 +0400

    commit

---
+++ level 01/homework/goa03.py
@@ -0,0 +1,13 @@
+#კომენტარი არის ის რასაც ვიყენებთ მესიჯებისთვის რაღაცის ასახსნელად და სხვადასხვა რაღაცეებისთვის
+#ფაილის გაფართოება არის ფაილის სახელის ბოლო  ნაწილი რომელიც წერტილის შემდეგ იწერება და მიუთითებს ფაილის ტიპს პითონის ფაილი რომ შევქმნათ  ფაილს უნდა ქონდეს .py დაბოლოვება
+#print ფუნქციას სხვადასხვა საიტებზე რაღაცა სიტყვილს გადასვლის ფუნქცია აქვს
+#სადაც კოდის შედეგებს ვხედავთ
+print("giorgi sulikashvili")
+print(12)
+print("home alone")
+print("home alone2")
+print("home alone3")
+print("home alone4")
+print("home alone5")
+print("giorgigiorgigiorgigiorgigiorgigiorgigiorgigiorgigiorgigiorgigiorgigiorgigi")
+print(17)
